385f71961876a9b38b0f2389f4eb3275de48d7da
Mathis Lefranc <lefrancmathis@gmail.com>
2022-05-28 12:22:35 -0400

    latest update

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,7 @@
 /*Toggle Button Script */
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
